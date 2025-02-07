127292e0e101ff2ce6fe7999b50b4d19310ab0d4
Max <marstonvisuals@gmail.com>
2025-02-07 17:53:35 +0000

    Add handle register route

--- service/user/routes.go
+++ service/user/routes.go
@@ -4,13 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/maximis3d/issue-tracking-system/types"
+	"github.com/maximis3d/issue-tracking-system/utils"
 )
 
 type Handler struct {
+	stre types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{ store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -19,9 +22,23 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
-
+	return nil
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+
+	var payload types.RegisterUserPayload
+
+	if err := utils.ParseJson(r.Body, payload), err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	// check if user exists
+	h.store.GetUserByEmail(payload.Email)
+
+
+
+	
+	// if not, create new user
 
 }
