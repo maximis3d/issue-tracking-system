b4da3c8c4c10dccba41191b4343ea61f0db0fb3b
Max <marstonvisuals@gmail.com>
2025-02-20 12:31:43 +0000

    Finish userregister func

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,9 +1,12 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
+	"github.com/maximis3d/issue-tracking-system/service/auth"
 	"github.com/maximis3d/issue-tracking-system/types"
 	"github.com/maximis3d/issue-tracking-system/utils"
 )
@@ -27,15 +30,42 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	// get JSON payload
-
-	var payload types.RegisterUserPayload
-
-	if err := utils.ParseJSON(r, payload); err != nil {
+	var user types.RegisterUserPayload
+	if err := utils.ParseJSON(r, &user); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
+		return
 	}
+
+	if err := utils.Validate.Struct(user); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
 	// check if user exists
+	_, err := h.store.GetUserByEmail(user.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", user.Email))
+		return
+	}
 
-	// if not, create new user
+	// hash password
+	hashedPassword, err := auth.HashPassword(user.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
 
+	err = h.store.CreateUser(types.User{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
