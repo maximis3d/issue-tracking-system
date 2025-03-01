fd047a60dd3667b78b279c916595c294f1d71545
Max <marstonvisuals@gmail.com>
2025-03-01 23:47:55 +0000

    Create React frontend application with vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className=''>
+        <h1>Issue Tracker</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
