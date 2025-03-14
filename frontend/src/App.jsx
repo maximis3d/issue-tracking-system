3e0df3d1221125589327e8f2b6bad20d90264fe0
maximis3d <marstonvisuals@gmail.com>
2025-03-14 15:36:33 +0000

    Add register route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignIn from "./components/loginForm/loginForm";
+import Register from "./components/registerForm/registerForm";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
