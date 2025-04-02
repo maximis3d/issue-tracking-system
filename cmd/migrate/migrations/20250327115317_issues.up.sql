14c44dfa96b9d719bcb2f6f58e95bf86ccf83c76
Max <marstonvisuals@gmail.com>
2025-04-02 10:51:35 +0100

    Add standups migrations and remove unnecessary comments from issues migrations`

--- cmd/migrate/migrations/20250327115317_issues.up.sql
+++ cmd/migrate/migrations/20250327115317_issues.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS issues (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `key` VARCHAR(255) NOT NULL UNIQUE,   -- The issue's unique key (e.g., PROJECT001-001)
+    `key` VARCHAR(255) NOT NULL UNIQUE,
     `summary` VARCHAR(255) NOT NULL,
     `description` TEXT NOT NULL,
-    `project_key` VARCHAR(255) NOT NULL,  -- Corrected column name here
+    `project_key` VARCHAR(255) NOT NULL,
     `reporter` VARCHAR(255) NOT NULL,
     `assignee` VARCHAR(255) NOT NULL,
     `status` ENUM('open', 'in_progress', 'resolved') NOT NULL DEFAULT 'open',
     `issueType` ENUM('bug', 'task', 'story') NOT NULL DEFAULT 'task',
     `createdAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updatedAt` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`project_key`) REFERENCES `projects`(`project_key`) ON DELETE CASCADE  -- Ensure matching column names here
+    FOREIGN KEY (`project_key`) REFERENCES `projects`(`project_key`) ON DELETE CASCADE
 );
