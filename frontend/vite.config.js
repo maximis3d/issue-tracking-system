fb850b37d1b86609d43c9a39d1d219f1a3835966
Max <marstonvisuals@gmail.com>
2025-04-24 12:12:20 +0100

    Revert vite.config.js due to broken tailwind config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// Export the configuration
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src', // This sets up the '@' alias to resolve to the src folder
-    },
-  },
   server: {
     host: true,
     port: 5173,
     watch: {
       usePolling: true,
     },
-  },
-});
+  }
+})
